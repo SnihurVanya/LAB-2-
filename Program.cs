c0f9ac0236f5215e8b614b3e7facccc42b0ba6c2
SnihurVanya <I.Snihur_FIT_12_22_B_d@knute.edu.ua>
2023-09-27 21:37:29 +0300

    lab2

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            //Створити укземпляр класу Address.
+            Address nt = new Address();
+
+            //У поля екземпляра записати інформаію про поштову адресу.
+            nt.Index = 07442;
+            nt.Country = "Ukraine";
+            nt.City = "Kyiv";
+            nt.Street = "Stepana Razina";
+            nt.House = "6";
+            nt.Apartment = "7";
+
+            //Виведіть на екран значення полів, що описують адресу.
+            Console.Out.WriteLine("Index; " + nt.Index);
+            Console.Out.WriteLine("Cointry: " + nt.Country);
+
+            //Створити екземпляр класу Address.
+            Address nt = new Address();
+
+            //У поля екземпляра записати інформацію про поштову адресу.
+            nt.Index = 07442;
+            nt.Country = "Ukraine";
+            nt.City = "Kyiv";
+            nt.Street = "Stepana Razina";
+            nt.House = "6";
+            nt.Apartment = "7";
+
+            //Виведіть на екран значення полів, що описують адресу.
+            Console.Out.WriteLine("Index: " + nt.Index);
+            Console.Out.WriteLine("Country: " + nt.Country);
+            Console.Out.WriteLine("City: " + nt.City);
+            Console.Out.WriteLine("Street: " + nt.Street);
+            Console.Out.WriteLine("House: " + nt.House);
+            Console.Out.WriteLine("Apartment: " + nt.Apartment);
+
+
+
+
+
+
+        }
+        static void Task2()
+        {
+            //Створити екземпляр класу Converter.
+            Converter conv = new Converter(36.96, 38.79, 8.37);
+            Console.WriteLine("What do you want to do?);
+    
+                string user_selection = Console.ReadLine();
+            int selection = Convert.ToInt16(user_selection);
+
+            
+            
+                
+            
+        }
+        static void Task4()
+        {
+            //Створити клас User
+            User user = new User("User", "Ivan", "Snihur", 18);
+            //Реалізуйте виведення на екран інформаії про користувача.
+            user.Output();
+        }
+          
+    }
+}
---
+++ User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class User
+    {
+        public string login { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime Date { get; }
+
+        public User(string login, string name, string surname, int age)//логін, ім'я, прізвище, вік, дату заповненняанкети
+        {
+            login = login;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Date = System.DateTime.Now;
+        }
+        public void Output()
+        {
+            Console.WriteLine("Login: " + login);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Date:" + date);
+        }
+            
+
+        
+    }
+}
